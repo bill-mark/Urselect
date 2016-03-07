369df16356b0da431dbb0a23658870775c95eceb
ancool <ancool@foxmail.com>
2016-03-07 23:43:32 +0800

    myorder

--- app/views/member/MyOrder.react.js
+++ app/views/member/MyOrder.react.js
@@ -5,14 +5,33 @@ import React, {
   Text,
   View,
   Image,
+  ScrollView,
 } from 'react-native'
+
+import ScrollableTabView, { DefaultTabBar, ScrollableTabBar } from 'react-native-scrollable-tab-view'
 import LoadingView from '../share/LoadingView.react' 
 
 export default class MyOrder extends Component {
     render(){
         return (
             <View style={styles.container}>
-                <Text>MyOrder</Text>
+                <ScrollableTabView initialPage={0} renderTabBar={() => <ScrollableTabBar />}>
+                    <ScrollView tabLabel='全部'>
+                        <Text>全部</Text>
+                    </ScrollView>
+                    <ScrollView tabLabel='待付款'>
+                        <Text>待付款</Text>
+                    </ScrollView>
+                    <ScrollView tabLabel='待发货'>
+                        <Text>待发货</Text>
+                    </ScrollView>
+                    <ScrollView tabLabel='待收货'>
+                        <Text>待收货</Text>
+                    </ScrollView>
+                    <ScrollView tabLabel='待评价'>
+                        <Text>待评价</Text>
+                    </ScrollView>
+                </ScrollableTabView>
             </View>
         )
     }
--- index.android.js
+++ index.android.js
@@ -6,29 +6,11 @@ import React, {
   StyleSheet,
   Text,
   View,
-  Dimensions,
 } from 'react-native'
 
 import RouteMapper from './app/RouteMapper.react'
 import NavigationBar from './app/NavigationBar.react'
-
-let SCREEN_WIDTH = Dimensions.get('window').width
-let BaseConfig = Navigator.SceneConfigs.FloatFromRight
-let CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
-  // 用户中断返回手势时，迅速弹回  
-  snapVelocity: 8,
-  // 如下设置可以使我们在屏幕的任何地方拖动它
-  edgeHitWidth: SCREEN_WIDTH,
-})
-let CustomSceneConfig = Object.assign({}, BaseConfig, {
-  // 如下设置使过场动画看起来很快
-  springTension: 500,
-  springFriction: 1,
-  // 使用上面我们自定义的手势
-  gestures: {
-    pop: CustomLeftToRightGesture,
-  }
-})
+import CustomSceneConfig from './app/CustomSceneConfig'
 
 class Urselect extends Component {
   render() {
